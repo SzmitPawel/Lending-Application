e1ac8257f5de7e9c7664173bacf66ced89c3c79c
“SzmitP” <szmitp@oriax.eu>
2023-07-07 20:31:41 +0200

    add Repayment entity and tests

---
+++ src/main/java/com/lending/application/domain/Repayment.java
@@ -0,0 +1,31 @@
+package com.lending.application.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "REPAYMENT")
+public class Repayment {
+    @Setter(AccessLevel.PRIVATE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true)
+    private Long repaymentId;
+    @Column(name = "REPAYMENT_AMOUNT", nullable = false)
+    private BigDecimal repaymentAmount;
+    @Column(name = "REPAYMENT_DATE", nullable = false)
+    private LocalDate repaymentDate;
+
+    public Repayment() {
+    }
+
+    public Repayment(final BigDecimal repaymentAmount, final LocalDate repaymentDate) {
+        this.repaymentAmount = repaymentAmount;
+        this.repaymentDate = repaymentDate;
+    }
+}
---
+++ src/test/java/com/lending/application/domain/RepaymentTest.java
@@ -0,0 +1,89 @@
+package com.lending.application.domain;
+
+import com.lending.application.repository.RepaymentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class RepaymentTest {
+    @Autowired
+    RepaymentRepository repaymentRepository;
+
+    @Test
+    void createRepaymentWithoutAmountAndDate_shouldReturnDataIntegrityViolationException() {
+        // given
+        Repayment repayment = new Repayment();
+
+        // when & then
+        assertThrows(DataIntegrityViolationException.class, () -> repaymentRepository.saveAndFlush(repayment));
+    }
+
+    @Test
+    void createRepayment_shouldReturn1() {
+        // given
+        Repayment repayment = new Repayment(new BigDecimal(100.00), LocalDate.now());
+
+        // when
+        repaymentRepository.saveAndFlush(repayment);
+
+        // then
+        assertEquals(1, repaymentRepository.count());
+    }
+
+    @Test
+    void deleteRepayment_shouldReturn0() {
+        // given
+        Repayment repayment = new Repayment(new BigDecimal(100.00), LocalDate.now());
+        repaymentRepository.saveAndFlush(repayment);
+
+        // when
+        repaymentRepository.deleteAll();
+
+        // then
+        assertEquals(0, repaymentRepository.count());
+    }
+
+    @Test
+    void updateRepayment_shouldReturnUpdatedData() {
+        // given
+        Repayment repayment = new Repayment(new BigDecimal(100.00), LocalDate.now());
+        repaymentRepository.saveAndFlush(repayment);
+
+        // when
+        repayment.setRepaymentAmount(new BigDecimal(200.00));
+        repayment.setRepaymentDate(LocalDate.of(2023,01,01));
+        repaymentRepository.saveAndFlush(repayment);
+
+        // then
+        assertEquals(new BigDecimal(200), repaymentRepository.findAll()
+                .get(0)
+                .getRepaymentAmount());
+        assertEquals(LocalDate.of(2023,01,01), repaymentRepository.findAll()
+                .get(0)
+                .getRepaymentDate());
+    }
+
+    @Test
+    void updateRepayment_shouldReturnAllData() {
+        // given
+        Repayment repayment = new Repayment(new BigDecimal(100.00), LocalDate.now());
+        repaymentRepository.saveAndFlush(repayment);
+
+        // when & then
+        assertEquals(new BigDecimal(100.00), repaymentRepository.findAll()
+                .get(0)
+                .getRepaymentAmount());
+        assertEquals(LocalDate.now(), repaymentRepository.findAll()
+                .get(0)
+                .getRepaymentDate());
+    }
+}
