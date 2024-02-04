23af362f25a2818517e28c4177adc4a21322eb48
“SzmitP” <szmitp@oriax.eu>
2024-02-04 22:15:30 +0100

    added Swagger and change @RequestParam to @PathVariable for variable id

--- src/test/java/com/lending/application/controller/CreditRatingControllerTest.java
+++ src/test/java/com/lending/application/controller/CreditRatingControllerTest.java
@@ -43,15 +43,13 @@ class CreditRatingControllerTest {
         return new CreditRatingDto(1L, CreditRatingEnum.TWO, LocalDate.now());
     }
 
-    private LinkedMultiValueMap<String,String> prepareParamsMap(
-            final Long clientId,
+    private LinkedMultiValueMap<String, String> prepareParamsMap(
             final BigDecimal income,
             final BigDecimal expenses
     ) {
-        LinkedMultiValueMap<String,String> linkedMultiValueMap = new LinkedMultiValueMap<>();
-        linkedMultiValueMap.add("clientId",clientId.toString());
-        linkedMultiValueMap.add("income",income.toString());
-        linkedMultiValueMap.add("expenses",expenses.toString());
+        LinkedMultiValueMap<String, String> linkedMultiValueMap = new LinkedMultiValueMap<>();
+        linkedMultiValueMap.add("income", income.toString());
+        linkedMultiValueMap.add("expenses", expenses.toString());
 
         return linkedMultiValueMap;
     }
@@ -59,13 +57,14 @@ class CreditRatingControllerTest {
     @Nested
     class CreateCreditRating {
         @Test
-        void testCreateCreditRating_shouldThrowClientNotFoundExceptionHttp404() throws Exception {
+        void create_credit_rating_should_return_http_status_not_found_404_if_client_not_found()
+                throws Exception {
             // given
-            Long clientId = 1L;
+            Long clientId = 999L;
             BigDecimal income = BigDecimal.TEN;
             BigDecimal expenses = BigDecimal.TEN;
 
-            LinkedMultiValueMap<String,String> linkedMultiValueMap = prepareParamsMap(clientId,income,expenses);
+            LinkedMultiValueMap<String, String> linkedMultiValueMap = prepareParamsMap(income, expenses);
 
             when(creditRatingFacade
                     .createNewCreditRating(any(), any(BigDecimal.class), any(BigDecimal.class)))
@@ -73,74 +72,78 @@ class CreditRatingControllerTest {
 
             // when & then
             mockMvc.perform(MockMvcRequestBuilders
-                            .post(PATH)
+                            .post(PATH + "/{clientId}", clientId)
                             .params(linkedMultiValueMap))
                     .andExpect(MockMvcResultMatchers.status().isNotFound());
         }
 
         @Test
-        void testCreateCreditRating_constraintViolationExceptionClientIdBelowMinHttp400() throws Exception {
+        void create_credit_rating_should_return_http_status_bad_request_400_if_client_id_is_below_min()
+                throws Exception {
             Long clientId = 0L;
             BigDecimal income = BigDecimal.TEN;
             BigDecimal expenses = BigDecimal.TEN;
 
-            LinkedMultiValueMap<String,String> linkedMultiValueMap = prepareParamsMap(clientId,income,expenses);
+            LinkedMultiValueMap<String, String> linkedMultiValueMap = prepareParamsMap(income, expenses);
 
             // when & then
             mockMvc.perform(MockMvcRequestBuilders
-                            .post(PATH)
+                            .post(PATH + "/clientId", clientId)
                             .params(linkedMultiValueMap))
                     .andExpect(MockMvcResultMatchers.status().isBadRequest());
         }
 
         @Test
-        void testCreateCreditRating_constraintViolationExceptionIncomeBelowMinHttp400() throws Exception {
+        void create_credit_rating_should_return_http_status_bad_request_400_if_income_is_below_min()
+                throws Exception {
             Long clientId = 1L;
             BigDecimal income = BigDecimal.ZERO;
             BigDecimal expenses = BigDecimal.TEN;
 
-            LinkedMultiValueMap<String,String> linkedMultiValueMap = prepareParamsMap(clientId,income,expenses);
+            LinkedMultiValueMap<String, String> linkedMultiValueMap = prepareParamsMap(income, expenses);
 
             // when & then
             mockMvc.perform(MockMvcRequestBuilders
-                            .post(PATH)
+                            .post(PATH + "/{clientId}", clientId)
                             .params(linkedMultiValueMap))
                     .andExpect(MockMvcResultMatchers.status().isBadRequest());
         }
 
         @Test
-        void testCreateCreditRating_constraintViolationExceptionExpensesBelowMinHttp400() throws Exception {
+        void create_credit_rating_should_return_http_status_bad_request_400_if_expenses_are_below_min()
+                throws Exception {
             Long clientId = 1L;
             BigDecimal income = BigDecimal.TEN;
             BigDecimal expenses = BigDecimal.ZERO;
 
-            LinkedMultiValueMap<String,String> linkedMultiValueMap = prepareParamsMap(clientId,income,expenses);
+            LinkedMultiValueMap<String, String> linkedMultiValueMap = prepareParamsMap(income, expenses);
 
             // when & then
             mockMvc.perform(MockMvcRequestBuilders
-                            .post(PATH)
+                            .post(PATH + "/{clientId}", clientId)
                             .params(linkedMultiValueMap))
                     .andExpect(MockMvcResultMatchers.status().isBadRequest());
         }
 
         @Test
-        void testCreateCreditRating_shouldReturnCreditRatingSucceedHttp200() throws Exception {
+        void create_credit_rating_should_return_http_status_ok_200_and_credit_rating_dto_if_succeed()
+                throws Exception {
             // given
             Long clientId = 1L;
             BigDecimal income = BigDecimal.valueOf(100.00);
             BigDecimal expenses = BigDecimal.valueOf(20.00);
 
-            LinkedMultiValueMap<String,String> linkedMultiValueMap = prepareParamsMap(clientId,income,expenses);
+            LinkedMultiValueMap<String, String> linkedMultiValueMap = prepareParamsMap(income, expenses);
 
             CreditRatingDto creditRatingDto = prepareCreditRatingDto();
 
             when(creditRatingFacade
-                    .createNewCreditRating(clientId,income,expenses))
+                    .createNewCreditRating(clientId, income, expenses))
                     .thenReturn(creditRatingDto);
 
             // when & then
             mockMvc.perform(MockMvcRequestBuilders
-                            .post(PATH)
+                            .post(PATH + "/{clientId}", clientId)
                             .params(linkedMultiValueMap))
                     .andExpect(MockMvcResultMatchers.status().isOk())
                     .andExpect(MockMvcResultMatchers.jsonPath("$.ratingId", Matchers.is(1)))
@@ -152,31 +155,34 @@ class CreditRatingControllerTest {
     @Nested
     class GetCreditRating {
         @Test
-        void testGetCreditRating_shouldThrowClientNotFoundExceptionHttp404() throws Exception {
+        void get_credit_rating_should_return_http_status_not_found_404_if_client_not_found()
+                throws Exception {
             // given
             Long clientId = 1L;
 
             when(creditRatingFacade.getCreditRating(clientId)).thenThrow(ClientNotFoundException.class);
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.get(PATH)
-                            .param("clientId",clientId.toString()))
+            mockMvc.perform(MockMvcRequestBuilders
+                            .get(PATH + "/{clientId}", clientId))
                     .andExpect(MockMvcResultMatchers.status().isNotFound());
         }
 
         @Test
-        void testGetCreditRating_constraintViolationExceptionClientIdBelowMinHttp400() throws Exception {
+        void get_credit_rating_should_return_http_status_bad_request_400_if_client_id_is_below_min()
+                throws Exception {
             // given
             Long clientId = 0L;
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.get(PATH)
-                            .param("clientId",clientId.toString()))
+            mockMvc.perform(MockMvcRequestBuilders
+                            .get(PATH + "/{clientId}", clientId))
                     .andExpect(MockMvcResultMatchers.status().isBadRequest());
         }
 
         @Test
-        void testGetCreditRating_SucceedHttp200() throws Exception {
+        void get_credit_rating_should_return_http_status_ok_200_if_succeed()
+                throws Exception {
             // given
             Long clientId = 1L;
             CreditRatingDto creditRatingDto = prepareCreditRatingDto();
@@ -184,7 +190,8 @@ class CreditRatingControllerTest {
             when(creditRatingFacade.getCreditRating(clientId)).thenReturn(creditRatingDto);
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.get(PATH).param("clientId",clientId.toString()))
+            mockMvc.perform(MockMvcRequestBuilders
+                            .get(PATH + "/{clientId}", clientId))
                     .andExpect(MockMvcResultMatchers.status().isOk())
                     .andExpect(MockMvcResultMatchers.jsonPath("$.ratingId", Matchers.is(1)))
                     .andExpect(MockMvcResultMatchers.jsonPath("$.creditRating", Matchers.is("TWO")))
@@ -196,7 +203,8 @@ class CreditRatingControllerTest {
     @Nested
     class UpdateCreditRating {
         @Test
-        void updateCreditRating_shouldThrowCreditRatingNotFoundException404() throws Exception {
+        void update_credit_rating_should_return_http_status_not_found_404_if_credit_rating_is_not_found()
+                throws Exception {
             // given
             CreditRatingDto creditRatingDto = prepareCreditRatingDto();
 
@@ -208,7 +216,8 @@ class CreditRatingControllerTest {
             String jsonContent = gson.toJson(creditRatingDto);
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.put(PATH)
+            mockMvc.perform(MockMvcRequestBuilders
+                            .put(PATH)
                             .contentType(MediaType.APPLICATION_JSON)
                             .characterEncoding(StandardCharsets.UTF_8)
                             .content(jsonContent))
@@ -216,7 +225,8 @@ class CreditRatingControllerTest {
         }
 
         @Test
-        void updateCreditRating_SucceedHttp200() throws Exception {
+        void update_credit_rating_should_return_http_status_ok_200_if_succeed()
+                throws Exception {
             // given
             CreditRatingDto creditRatingDto = new CreditRatingDto();
             creditRatingDto.setRatingId(1L);
@@ -231,7 +241,8 @@ class CreditRatingControllerTest {
             String jsonContent = gson.toJson(creditRatingDto);
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.put(PATH)
+            mockMvc.perform(MockMvcRequestBuilders
+                            .put(PATH)
                             .contentType(MediaType.APPLICATION_JSON)
                             .characterEncoding(StandardCharsets.UTF_8)
                             .content(jsonContent))
@@ -246,37 +257,40 @@ class CreditRatingControllerTest {
     @Nested
     class DeleteCreditRating {
         @Test
-        void testDeleteCreditRating_CreditRatingNotFoundException404() throws Exception {
+        void delete_credit_rating_should_return_http_status_not_found_404_if_credit_rating_is_not_found()
+                throws Exception {
             // given
             Long creditRatingId = 1L;
 
             willThrow(CreditRatingNotFoundException.class).given(creditRatingFacade).deleteCreditRating(any());
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.delete(PATH)
-                            .param("creditRatingId",creditRatingId.toString()))
+            mockMvc.perform(MockMvcRequestBuilders
+                            .delete(PATH + "/{creditRatingId}", creditRatingId))
                     .andExpect(MockMvcResultMatchers.status().isNotFound());
         }
 
         @Test
-        void testDeleteCreditRating_constraintViolationExceptionRatingIdBelowMinHttp400() throws Exception {
+        void delete_credit_rating_should_return_http_status_bad_request_400_if_credit_rating_id_is_below_min()
+                throws Exception {
             // given
             Long creditRatingId = 0L;
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.delete(PATH)
-                            .param("creditRatingId",creditRatingId.toString()))
+            mockMvc.perform(MockMvcRequestBuilders
+                            .delete(PATH + "/{creditRatingId}", creditRatingId))
                     .andExpect(MockMvcResultMatchers.status().isBadRequest());
         }
 
         @Test
-        void testDeleteCreditRating_SucceedHttp200() throws Exception {
+        void delete_credit_rating_should_return_http_status_ok_200_if_succeed()
+                throws Exception {
             // given
             Long creditRatingId = 1L;
 
             // when & then
-            mockMvc.perform(MockMvcRequestBuilders.delete(PATH)
-                            .param("creditRatingId",creditRatingId.toString()))
+            mockMvc.perform(MockMvcRequestBuilders
+                            .delete(PATH + "/{creditRatingId}", creditRatingId))
                     .andExpect(MockMvcResultMatchers.status().isOk());
         }
     }
